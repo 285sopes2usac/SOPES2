05534bf036413cc5ff70b93f82a66f185a6ae0d3
Steven Jocol <53009062+Stevengez@users.noreply.github.com>
2025-08-08 00:20:58 -0600

    Update syscall_usac.c

--- Ejemplo Nueva Syscall/kernel/syscall_usac.c
+++ Ejemplo Nueva Syscall/kernel/syscall_usac.c
@@ -1,7 +1,23 @@
-#include <linux/syscalls.h>
+#include <linux/syscalls.h> // Para usar macros de llamadas al sistema
+// #include <linux/syscall_usac.h> // Incluir el header de la syscall si se usa la creacion manual, sin macros
 
-SYSCALL_DEFINE0(usac_hello)
+// SYSCALL_DEFINE0 es una macro que define una syscall sin argumentos
+// El 0 indica que no se esperan argumentos para esta syscall
+// Se puede usar desde el 0 hasta el 6 para definir syscalls con hasta 6 argumentos
+// Este limite de 6 argumentos es por el uso de registros en la arquitectura x86_64
+
+// sys_usac_hello es el nombre de la syscall que se va a implementar
+SYSCALL_DEFINE0(usac_hello) // Definición de syscall sin argumentos
 {
-    printk(KERN_INFO "USAC syscall ejecutada\n");
+    printk(KERN_INFO "USAC Syscall ejecutada\n");
+    // Alternativa usar macro para log nivel info: // pr_info("Hello from usac syscall!\n");
     return 42;
 }
+
+// Forma alterna de crear la implementacion sin usar macros
+// El nombre de la funcion debe ser igual a la definida en la cuarta columna
+// de la tabla de syscalls agregando el prefijo __ARCH_, ej: __x64_
+// asmlinkage long __x64_sys_usac_hello(void){
+//     printk(KERN_INFO "USAC Syscall ejecutada\n");
+//     return 42;
+// }
